ae47c0868ab8b5fd1a5d09a08176cac9c28b3cad
Raghad <raghadalfuqaha2000@gmail.com>
2023-10-23 12:36:51 +0300

    update

---
+++ src/Magento_signin.java
@@ -0,0 +1,64 @@
+import java.util.Random;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Magento_signin {
+	WebDriver driver = new ChromeDriver ();
+	
+	Random rand = new Random();
+
+	String URL = "https://magento.softwaretestingboard.com/customer/account/create/";
+	String FirstName = "Raghad";
+	String lastName = "alfuqaha";
+	String Email = "raghad";
+	String Domain = "@gmail.com";
+	String Password = "raghad445566";
+
+	@BeforeTest
+	public void beforeTest () {
+		
+		driver.get(URL);
+	}
+	
+	
+	@Test(invocationCount = 10)
+	public void myTest () {
+		int myNewIDForTheEmail = rand.nextInt(10000);
+		// define all the webelement that will be used
+		WebElement FirstNameInputField = driver.findElement(By.id("firstname"));
+		WebElement LastNameInputField = driver.findElement(By.id("lastname"));
+		WebElement EmailInputField = driver.findElement(By.id("email_address"));
+		WebElement PasswordInputField = driver.findElement(By.id("password"));
+		WebElement PasswordConfirmationField = driver.findElement(By.id("password-confirmation"));
+
+		// interact with webelements
+		FirstNameInputField.sendKeys(FirstName);
+		LastNameInputField.sendKeys(lastName);
+		EmailInputField.sendKeys(Email + myNewIDForTheEmail + Domain);
+		PasswordInputField.sendKeys(Password);
+		PasswordConfirmationField.sendKeys(Password);
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	@AfterTest
+	public void myAftertest () {
+		
+		
+	}
+	
+	
+
+}
